e4f13ffc8cc1816eb756f82db94ee0a80dabbaba
SamBarton <sambarton@live.com>
2016-10-01 12:33:27 -0400

    Uh?

--- Nightmare/app/src/main/java/com/example/android/nightmare/MainActivity.java
+++ Nightmare/app/src/main/java/com/example/android/nightmare/MainActivity.java
@@ -6,10 +6,12 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.view.View.OnClickListener;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -20,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Button newTimeTest = (Button) findViewById(R.id.NewTimeTestButton);
 
     }
 
@@ -46,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //for newtime testing
-    public void newTimeTest(){
+    public void newTimeTest( View v ){
         NewTimeActivity newTimeActivity = new NewTimeActivity();
         newTimeActivity.newTime();
     }
