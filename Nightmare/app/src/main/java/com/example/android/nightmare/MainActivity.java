d2fd04bce965500668b7f9a7432e2158cbe81fd1
SamBarton <sambarton@live.com>
2016-10-01 01:02:58 -0400

    IDK what to put

--- Nightmare/app/src/main/java/com/example/android/nightmare/MainActivity.java
+++ Nightmare/app/src/main/java/com/example/android/nightmare/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -48,5 +50,37 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    void newTime() {
+        /*
+         *  Hour spinner
+         */
+        Spinner hour = (Spinner) findViewById(R.id.hour);
+        ArrayAdapter<CharSequence> hourAdapter = ArrayAdapter.createFromResource(this.getApplicationContext(),
+                R.array.hour, android.R.layout.simple_spinner_item);
+        hourAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        hour.setAdapter(hourAdapter);
+
+        /*
+         *  Min spinner
+         */
+        Spinner min = (Spinner) findViewById(R.id.min);
+        ArrayAdapter<CharSequence> minAdapter = ArrayAdapter.createFromResource(this.getApplicationContext(),
+                R.array.min, android.R.layout.simple_spinner_item);
+        minAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        min.setAdapter(minAdapter);
+
+        /*
+         *  AM/PM
+         */
+        /*
+         *  Hour spinner
+         */
+        Spinner amPm = (Spinner) findViewById(R.id.amPm);
+        ArrayAdapter<CharSequence> amPmAdapter = ArrayAdapter.createFromResource(this.getApplicationContext(),
+                R.array.amPm, android.R.layout.simple_spinner_item);
+        amPmAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        amPm.setAdapter(hourAdapter);
     }
 }
